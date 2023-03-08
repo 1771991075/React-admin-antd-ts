8d7aa70039b72a44e77918f3ca012c1e3c038c06
lzc <lvzicong123@outlook.com>
2023-03-08 17:12:59 +0800

    配置类型

--- src/type/user.d.ts
+++ src/type/user.d.ts
@@ -2,3 +2,28 @@ interface UserLoginParams {
     username:string,
     password:string
 }
+
+interface UserReduxType {
+    username:string | null,
+    token:string | null
+}
+
+interface UserReduxActions {
+    type:string,
+    playload:UserReduxType
+}
+
+interface MenusType {
+    icon:JSX.Element,
+    key:number | string,
+    label:string,
+    children?:MenusType[]
+}
+
+interface MenuListType {
+    authName:string,
+    id:number,
+    order:number,
+    path:string,
+    children:MenuListType[]
+}
