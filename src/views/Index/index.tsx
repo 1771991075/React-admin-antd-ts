b81a4784fb65b9e7b51975d6e8b5595dd52beb1c
lzc <lvzicong123@outlook.com>
2023-03-08 19:27:00 +0800

    退出登录

--- src/views/Index/index.tsx
+++ src/views/Index/index.tsx
@@ -1,23 +1,42 @@
 import React, { useState } from 'react';
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
-import { Layout, Menu, theme ,Button } from 'antd';
+import { Layout, Menu, theme, Button, Modal } from 'antd';
 import kun from '../../assets/kunkun.jpg';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom'
 import './index.css'
 //引入自定义hooks
 import useMenus from './hooks/useMenus';
 const { Header, Sider, Content } = Layout;
 
 const Index: React.FC = () => {
+  let navigate = useNavigate()
   //左侧导航栏显示隐藏
   const [collapsed, setCollapsed] = useState(false);
+  // 退出对话框
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => { setIsModalOpen(true) };
+  //退出登录
+  const handleOk = () => {
+    localStorage.removeItem('token')
+    setIsModalOpen(false)
+    navigate('/login')
+  };
+  //取消退出登录
+  const handleCancel = () => {
+    setIsModalOpen(false)
+  };
   const {
     token: { colorBgContainer },
   } = theme.useToken();
-  //从仓库中拿到username
+  // 从仓库中拿到username
   let username = useSelector((state: StateType) => {
     return state.userReducer.username
   })
+  // 退出登录
+  let exit = () => {
+    showModal()
+  }
 
   // 调用自定义hooks获取 items
   let items = useMenus()
@@ -43,7 +62,10 @@ const Index: React.FC = () => {
           {
             !collapsed && <MenuFoldOutlined style={{ fontSize: '20px' }} onClick={() => setCollapsed(!collapsed)} />
           }
-          <Button type="primary" onClick={()=>localStorage.removeItem('token')}>退出</Button>
+          <Button type="primary" onClick={exit}>退出</Button>
+          <Modal title="提示" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+            <p>是否退出登录?</p>
+          </Modal>
         </Header>
         <Content
           style={{
