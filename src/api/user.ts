c224295e67f37153bc04b8b1031f1218bd0e2f9b
lzc <lvzicong123@outlook.com>
2023-03-08 17:11:37 +0800

    添加请求api

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,6 +2,10 @@ import http from "../utils/http";
 // 用户登录
 let userLogin = (data:UserLoginParams):Promise<any>=> http(`login`,"post",data);
 
+// 获取首页左侧导航栏列表
+let getMenus = ():Promise<any>=> http(`menus`,'get')
+
 export {
-    userLogin
+    userLogin,
+    getMenus
 }
