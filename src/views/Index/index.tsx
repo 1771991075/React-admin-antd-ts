9259d10cd5ad07963a5061c84700cd0dec22ddb4
lzc <lvzicong123@outlook.com>
2023-03-09 14:13:51 +0800

    面包屑导航

--- src/views/Index/index.tsx
+++ src/views/Index/index.tsx
@@ -1,19 +1,46 @@
-import React, { useState } from 'react';
-import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
-import { Layout, Menu, theme, Button, Modal } from 'antd';
+import React, { useEffect, useState ,useMemo} from 'react';
+import { MenuFoldOutlined, MenuUnfoldOutlined ,HomeOutlined } from '@ant-design/icons';
+import { Layout, Menu, theme, Button, Modal, Breadcrumb } from 'antd';
 import kun from '../../assets/kunkun.jpg';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation ,NavLink } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
+import navList from '../../utils/navList';
 import './index.css';
 //引入自定义hooks
 import useMenus from './hooks/useMenus';
 const { Header, Sider, Content } = Layout;
 
 const Index: React.FC = () => {
+  let location = useLocation()
   let navigate = useNavigate()
   //左侧导航栏显示隐藏
   const [collapsed, setCollapsed] = useState(false);
+  //定义默认展开的一级菜单key列表
+  let [defaultOpenKeys, setDefaultOpenKeys] = useState<string[]>();
+  // 定义默认选中的二级菜单key列表
+  let [defaultSelectedKeys, setDefaultSelectedKeys] = useState<string[]>();
+  // 获取navList状态
+  let navlist = useSelector((state:StateType)=>{
+    return state.navReducer.navList
+  })
+  // useMemo 处理navList
+  let Items = useMemo(()=>{
+    let list:Items[] = [{
+      title: <NavLink to='home'><HomeOutlined /></NavLink>,
+    }]
+    if(navlist === undefined){
+      navlist = []
+    }
+    navlist.forEach(item=>{
+      let obj = {
+        href:'',
+        title:item
+      }
+      list.push(obj)
+    })
+    return list
+  },[navlist])
   // 退出对话框
   const [isModalOpen, setIsModalOpen] = useState(false);
   const showModal = () => { setIsModalOpen(true) };
@@ -39,6 +66,37 @@ const Index: React.FC = () => {
     showModal()
   }
 
+  useEffect(() => {
+    // 获取当前二级路由路径
+    let pathName = location.pathname.split('/')[2]
+    setDefaultSelectedKeys([pathName])
+    // 根据当前路由获取对应navList
+
+    // 根据二级路由路径更新默认展开菜单
+    switch (pathName) {
+      case 'users':
+        setDefaultOpenKeys(["125"])
+        break;
+      case 'roles':
+        setDefaultOpenKeys(["103"])
+        break
+      case 'rights':
+        setDefaultOpenKeys(["103"])
+        break
+      case 'orders':
+        setDefaultOpenKeys(["102"])
+        break;
+      case 'reports':
+        setDefaultOpenKeys(["145"])
+        break;
+      case 'home':
+        setDefaultOpenKeys([])
+        break;
+      default:
+        setDefaultOpenKeys(["101"])
+    }
+  }, [location])
+
   // 调用自定义hooks获取 items
   let items = useMenus()
   return (
@@ -51,9 +109,28 @@ const Index: React.FC = () => {
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={['1']}
+          openKeys={defaultOpenKeys}
+          selectedKeys={defaultSelectedKeys}
           items={items}
-          onSelect={(keyPath) => { navigate(`/index/${keyPath.key}`) }}
+          onClick={(item) => {
+            console.log(item);
+            // 跳转对应页面
+            navigate(item.key)
+            // 更新当前选中的二级菜单
+            setDefaultSelectedKeys([item.key])
+            // 更新当前展开的一级菜单
+            setDefaultOpenKeys([item.keyPath[1]])
+            // 根据当前路由获取对应navlist
+
+          }}
+          // 展开一级菜单时调用
+          onOpenChange = {(openKeys)=>{
+            if(openKeys.length === 1){
+              setDefaultOpenKeys(openKeys)
+              return
+            }
+            setDefaultOpenKeys([openKeys[1]])
+          }}
         />
       </Sider>
       <Layout className="site-layout">
@@ -69,15 +146,16 @@ const Index: React.FC = () => {
             <p>是否退出登录?</p>
           </Modal>
         </Header>
+        <Breadcrumb style={{padding:'10px 20px'}} items={Items} />
         <Content
           style={{
-            margin: '24px 16px',
+            margin: '0px 16px 24px 16px',
             padding: 24,
             minHeight: 280,
             background: colorBgContainer,
           }}
         >
-          <Outlet/>
+          <Outlet />
         </Content>
       </Layout>
     </Layout>
