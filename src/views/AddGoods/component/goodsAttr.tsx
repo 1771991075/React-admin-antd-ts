8b56fc148671a3e88fe97fc7593667fc2deb0700
lzc <lvzicong123@outlook.com>
2023-03-14 16:01:49 +0800

    渲染商品静态分类列表

--- src/views/AddGoods/component/goodsAttr.tsx
+++ src/views/AddGoods/component/goodsAttr.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import { Form, Input } from 'antd'
+interface PropsType {
+  attrList: AttrsType1[];
+}
 
-export default function goodsAttr() {
+function GoodsAttr(props: PropsType) {
+  let { attrList } = props;
   return (
-    <div>goodsAttr</div>
+    <div>
+      <Form
+        name="basic"
+        labelCol={{ span: 4 }}
+        style={{ width: '100%' }}
+        autoComplete="off"
+        layout="vertical"
+      >
+        {
+          attrList.map((item: AttrsType1, index: number) => {
+            return (
+              <Form.Item
+                label={item.attr_name}
+                name={item.attr_id}
+                key={index}
+                initialValue={item.attr_vals}
+              >
+                <Input />
+              </Form.Item>
+            )
+          })
+        }
+      </Form>
+    </div>
   )
 }
+
+export default GoodsAttr;
