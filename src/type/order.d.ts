01af0c051d6c21de6b774e0e28abb7f5bd954785
lzc <lvzicong123@outlook.com>
2023-03-16 11:01:25 +0800

    完善订单页面

--- src/type/order.d.ts
+++ src/type/order.d.ts
@@ -1,3 +1,4 @@
+//请求订单列表数据类型
 interface GetOrderList {
     pagenum:number,
     pagesize:number,
@@ -9,4 +10,22 @@ interface GetOrderList {
     order_fapiao_company?:string,
     order_fapiao_content?:string,
     consignee_addr?:string
+}
+
+//订单详情
+interface OrderInfoType {
+    order_id:number,
+    user_id:number,
+    order_number:string,
+    order_price:number,
+    order_pay:string,
+    is_send:string,
+    trade_no?:string,
+    order_fapiao_title:string,
+    order_fapiao_company?:string,
+    order_fapiao_content:string,
+    consignee_addr:string,
+    pay_status:string,
+    create_time:number,
+    update_time:number
 }
--- src/views/Orders/index.tsx
+++ src/views/Orders/index.tsx
@@ -1,38 +1,49 @@
-import { AudioOutlined } from '@ant-design/icons';
-import { Input, Space, Select, Table, Tag, Button } from 'antd';
+import { Input, Select, Table, Tag, Button ,Pagination} from 'antd';
+import type { PaginationProps } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import { FormOutlined, SettingOutlined } from '@ant-design/icons';
+import { getOrderList } from '../../api/orders';
+import { useEffect, useState } from 'react';
 const { Search } = Input;
 export default function Orders() {
-
+  //表格数据
+  let [data, setData] = useState<OrderInfoType[]>([])
+  //页码
+  let [pagenum, setPagenum] = useState<number>(1)
+  //每页展示数量
+  let [pagesize, setPagesize] = useState<number>(5)
+  //总共条数
+  let [total, setTotal] = useState(0)
+  //搜索的内容
+  let [query, setQuery] = useState<string>('')
   //搜索框改变
-  const onSearch = (value: string) => console.log(value);
+  const onSearch = (value: string) => setQuery(value);
   //支付状态
   const handleChange = (value: string) => {
     console.log(`selected ${value}`);
   };
   //表格表头
-  const columns: ColumnsType<DataType> = [
+  const columns: ColumnsType<OrderInfoType> = [
     {
       title: '#',
       dataIndex: 'index',
-      width:30
+      width: 30
     },
     {
       title: '订单编号',
-      dataIndex: 'age',
+      dataIndex: 'order_number',
     },
     {
       title: '订单价格',
-      dataIndex: 'address',
+      dataIndex: 'order_price',
     },
     {
       title: '是否付款',
-      dataIndex: 'address',
+      dataIndex: 'order_pay',
     },
     {
       title: '是否发货',
-      dataIndex: 'address',
+      dataIndex: 'is_send',
       render() {
         return (
           <Tag color={'blue'}>银行卡</Tag>
@@ -41,22 +52,43 @@ export default function Orders() {
     },
     {
       title: '下单时间',
-      dataIndex: 'address',
+      dataIndex: 'create_time',
     },
     {
       title: '操作',
       key: 'action',
       render: (_, record) => (
         <>
-          <Button type='primary' icon={<FormOutlined/>}>修改订单状态</Button>
-          <Button type='primary' icon={<SettingOutlined/>}>物流信息</Button>
-          <Button type='primary' icon={<SettingOutlined/>}>订单详情</Button>
+          <Button type='primary' size='small' icon={<FormOutlined />}>修改订单状态</Button>&nbsp;
+          <Button type='primary' size='small' icon={<SettingOutlined />}>物流信息</Button>&nbsp;
+          <Button type='primary' size='small' icon={<SettingOutlined />}>订单详情</Button>
         </>
       ),
     },
   ];
-  //表格数据
-  const data: DataType[] = []
+  //点击切换页码
+  let onChange: PaginationProps['onChange'] = (page, pageSize) => {
+    setPagenum(page)
+    setPagesize(pageSize)
+  };
+
+  //获取订单列表
+  let getOrder = async () => {
+    let data = {
+      pagenum,
+      pagesize,
+      query
+    }
+    let res = await getOrderList(data)
+    if (res.data.meta.status === 200) {
+      setData(res.data.data.goods)
+      setTotal(res.data.data.total)
+    }
+  }
+
+  useEffect(() => {
+    getOrder()
+  },[pagenum,pagesize,query])
 
   return (
     <div>
@@ -82,8 +114,9 @@ export default function Orders() {
           ]}
         />
       </div>
-      <div style={{marginTop:'20px'}}>
-        <Table columns={columns} dataSource={data} bordered />
+      <div style={{ marginTop: '20px' }}>
+        <Table columns={columns} dataSource={data} bordered rowKey={(record): any => record.order_id} pagination={false} />
+        <Pagination showQuickJumper defaultPageSize={pagesize} current={pagenum} total={total} pageSizeOptions={[5, 10, 20, 50]} onChange={onChange} showSizeChanger style={{ marginTop: '20px' }} />
       </div>
     </div>
   )
